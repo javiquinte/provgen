6bc76b53fb23e1b420b705303e38a2bbb79476d3
Javier Quinteros <javier@quinteros.eu>
2017-10-02 11:56:05 +0200

    Lots of python2 to python3 modifications.

--- test/testService.py
+++ test/testService.py
@@ -21,14 +21,13 @@ import sys
 import os
 import datetime
 import unittest
-import urllib2
-from urlparse import urlparse
+from urllib.request import Request
+from urllib.request import urlopen
+from urllib.request import HTTPError
+from urllib.parse import urlparse
 import json
 from difflib import Differ
 from xml.dom.minidom import parseString
-
-here = os.path.dirname(__file__)
-sys.path.append(os.path.join(here, '..'))
 from unittestTools import WITestRunner
 
 
@@ -44,12 +43,12 @@ class ProvgenTests(unittest.TestCase):
         """Very large URI."""
         msg = 'A URI of more than 2000 characters is not allowed and ' + \
             'should return a 414 error code'
-        req = urllib2.Request('%s?net=GE%s' % (self.host, '&net=GE' * 500))
+        req = Request('%s?net=GE%s' % (self.host, '&net=GE' * 500))
         try:
-            u = urllib2.urlopen(req)
+            u = urlopen(req)
             u.read()
-        except urllib2.URLError as e:
-            self.assertEqual(e.code, 414, msg)
+        except HTTPError as e:
+            self.assertEqual(e.getcode(), 414, msg)
             return
 
         self.assertTrue(False, msg)
@@ -59,12 +58,12 @@ class ProvgenTests(unittest.TestCase):
         """Unknown parameter."""
         msg = 'An error code 400 Bad Request is expected for an unknown ' + \
             'parameter'
-        req = urllib2.Request('%s?net=GE&wrongparam=1' % self.host)
+        req = Request('%s?net=GE&wrongparam=1' % self.host)
         try:
-            u = urllib2.urlopen(req)
+            u = urlopen(req)
             u.read()
-        except urllib2.URLError as e:
-            self.assertEqual(e.code, 400, msg)
+        except HTTPError as e:
+            self.assertEqual(e.getcode(), 400, msg)
             return
 
         self.assertTrue(False, msg)
@@ -72,15 +71,14 @@ class ProvgenTests(unittest.TestCase):
 
     def test_wrong_format(self):
         """Wrong format option."""
-        req = urllib2.Request('%s?net=GE&format=WRONGFORMAT' %
-                              self.host)
+        req = Request('%s?net=GE&format=WRONGFORMAT' % self.host)
         msg = 'When a wrong format is specified an error code 400 is expected!'
         try:
-            u = urllib2.urlopen(req)
+            u = urlopen(req)
             u.read()
-        except urllib2.URLError as e:
+        except HTTPError as e:
             if hasattr(e, 'code'):
-                self.assertEqual(e.code, 400, '%s (%s)' % (msg, e.code))
+                self.assertEqual(e.getcode(), 400, '%s (%s)' % (msg, e.code))
                 return
 
             self.assertTrue(False, '%s (%s)' % (msg, e))
@@ -91,14 +89,14 @@ class ProvgenTests(unittest.TestCase):
 
     def test_version(self):
         """'version' method."""
-        if self.host.endswith('query'):
-            vermethod = '%sversion' % self.host[:-len('query')]
+        if self.host.endswith('/'):
+            vermethod = '%sversion' % self.host
         else:
-            pass
+            raise Exception('Wrong service URL format. A / is expected as last character.')
 
-        req = urllib2.Request(vermethod)
+        req = Request(vermethod)
         try:
-            u = urllib2.urlopen(req)
+            u = urlopen(req)
             buffer = u.read()
         except:
             raise Exception('Error retrieving version number')
@@ -119,7 +117,7 @@ class ProvgenTests(unittest.TestCase):
 # ----------------------------------------------------------------------
 def usage():
     """Print how to use the service test."""
-    print 'testService [-h|--help] [-p|--plain] http://server/path'
+    print('testService [-h|--help] [-p|--plain] http://server/path')
 
 
 global host
