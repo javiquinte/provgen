40ecd68fa6220c7435b134b00d5af9342ceb9faf
Javier Quinteros <javier@quinteros.eu>
2017-11-15 15:10:34 +0100

    Escape content of variables. The problem appeared with the filepaths.

--- provgen.py
+++ provgen.py
@@ -29,6 +29,7 @@
 import cherrypy
 import os
 import json
+import re
 
 try:
     import configparser
@@ -58,7 +59,7 @@ class TemplatesAPI(object):
             # Keep the specification in a list
             wholetemp = fin.read()
             for key, value in params.items():
-                wholetemp = wholetemp.replace('{%s:%s}' % (prefix, key), value)
+                wholetemp = wholetemp.replace('{%s:%s}' % (prefix, key), re.escape(value))
 
             # Check that all variables have been replaced
 
